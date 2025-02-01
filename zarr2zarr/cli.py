bc0ec738c79674fcb1f16b50baba6ba15e310dfd
Matthew Hartley <matthewh@ebi.ac.uk>
2025-02-01 18:47:34 +0000

    feat: Add thumbnail generation to CLI info command

--- zarr2zarr/cli.py
+++ zarr2zarr/cli.py
@@ -1,6 +1,8 @@
+import rich
 import typer
 
 from .proxyimage import open_ome_zarr_image
+from .rendering import generate_padded_thumbnail_from_ngff_uri
 
 
 app = typer.Typer()
@@ -8,7 +10,8 @@ app = typer.Typer()
 
 @app.command()
 def info(ome_zarr_uri: str):
-    open_ome_zarr_image(ome_zarr_uri)
+    im = generate_padded_thumbnail_from_ngff_uri(ome_zarr_uri)
+    im.save('foo.png')
 
 
 if __name__ == "__main__":
