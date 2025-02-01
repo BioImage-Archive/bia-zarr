45826f265cb5640da19f8f4eb3dda6a6c44bca31
Matthew Hartley <matthewh@ebi.ac.uk>
2025-02-01 18:03:47 +0000

    refactor: Modify OME Zarr image handling and metadata parsing

--- zarr2zarr/proxyimage.py
+++ zarr2zarr/proxyimage.py
@@ -1,14 +1,51 @@
 import zarr
-from ome_zarr_models import open_ome_zarr # type: ignore
 
+from .omezarrmeta import OMEZarrMeta
+
+# NOTE - replace with the ome zarr models version once v3 support exists
+def open_ome_zarr(zarr_group: zarr.Group):
+    attrs_dict = zarr_group.attrs.asdict()
+
+    image_metadata = OMEZarrMeta.parse_obj(attrs_dict["ome"])
+
+    import rich
+    rich.print(image_metadata)
+
+
+class OMEZarrImage(BaseModel):
+
+    sizeX: int
+    sizeY: int
+    sizeZ: int = 1
+    sizeC: int = 1
+    sizeT: int = 1
+
+    # Valid values for dimensions are {'tczyx', 'zyx', 'tcyx', 'czyx'}
+    dimensions: str = "tczyx"
+    zgroup: zarr.Group
+
+    n_scales: int = 1
+    xy_scaling: float = 1.0
+    z_scaling: float = 1.0
+    path_keys: List[str]= []
+        
+    PhysicalSizeX: Optional[float] = None
+    PhysicalSizeY: Optional[float] = None
+    PhysicalSizeZ: Optional[float] = None
+
+    ngff_metadata: ZMeta | None = None
+
+    class Config:
+        arbitrary_types_allowed=True
 
 
 def open_ome_zarr_image(ome_zarr_image_uri: str):
     zarr_group = zarr.open_group(ome_zarr_image_uri)
 
-    image_metadata = open_ome_zarr(zarr_group).attributes
+    open_ome_zarr(zarr_group)
+    # image_metadata = open_ome_zarr(zarr_group).attributes
 
-    input_array = zarr_group[image_metadata.multiscales[0].datasets[0].path]
-    import rich
-    rich.print(input_array)
+    # input_array = zarr_group[image_metadata.multiscales[0].datasets[0].path]
+    # import rich
+    # rich.print(input_array)
 
