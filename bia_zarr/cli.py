499d0eedaf9963f8ed7f5f73f501c00ac86044f9
Matthew Hartley <matthewh@ebi.ac.uk>
2025-02-08 13:28:16 +0000

    feat: Add new CLI commands for spike write, validate thing, and modify existing commands

--- bia_zarr/cli.py
+++ bia_zarr/cli.py
@@ -10,20 +10,45 @@ app = typer.Typer()
 
 
 @app.command()
-def validate_ome_zarr(ome_zarr_uri: str):
-    ome_zarr_image = open_ome_zarr_image(ome_zarr_uri)
+def spike_write(url: str):
+    ome_zarr_image = open_ome_zarr_image(url)
+    from .zarr2zarr import write_array_to_disk_chunked
+    import tensorstore as ts
+
+    input_array_url = url + '/' + ome_zarr_image.path_keys[0]
+    # array = ome_zarr_image.zarr_group[ome_zarr_image.path_keys[0]]
+    source = ts.open({
+        'driver': 'zarr',
+        'kvstore': input_array_url,
+    }).result()
+
+    target_chunks = [1, 1, 64, 64, 64]
+
+    write_array_to_disk_chunked(source,  'goo', target_chunks)
+
+
+@app.command()
+def validate_ome_zarr_thing(url: str):
+    from .thing import open_ome_zarr_thing
+
+    open_ome_zarr_thing(url)
+
+
+@app.command()
+def validate_ome_zarr_image(ome_zarr_url: str):
+    ome_zarr_image = open_ome_zarr_image(ome_zarr_url)
 
     rich.print(ome_zarr_image)
 
 
 @app.command()
 def thumbnail(
-    ome_zarr_uri: str,
+    ome_zarr_url: str,
     output: str = typer.Option(..., help="Output filename"),
     dimensions: Tuple[int, int] = typer.Option((256, 256), help="Output dimensions (width, height)")
 ):
     """Generate a thumbnail from an OME-ZARR image with specified dimensions."""
-    im = generate_padded_thumbnail_from_ngff_uri(ome_zarr_uri, dims=dimensions)
+    im = generate_padded_thumbnail_from_ngff_uri(ome_zarr_url, dims=dimensions)
     im.save(output)
 
 
