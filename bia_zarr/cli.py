7b878401084ac08c529faa0e901a065af13ea9b0
Matthew Hartley <matthewh@ebi.ac.uk>
2025-02-08 13:52:11 +0000

    Remove redundant code

--- bia_zarr/cli.py
+++ bia_zarr/cli.py
@@ -10,24 +10,6 @@ app = typer.Typer()
 
 
 @app.command()
-def spike_write(url: str):
-    ome_zarr_image = open_ome_zarr_image(url)
-    from .zarr2zarr import write_array_to_disk_chunked
-    import tensorstore as ts
-
-    input_array_url = url + '/' + ome_zarr_image.path_keys[0]
-    # array = ome_zarr_image.zarr_group[ome_zarr_image.path_keys[0]]
-    source = ts.open({
-        'driver': 'zarr',
-        'kvstore': input_array_url,
-    }).result()
-
-    target_chunks = [1, 1, 64, 64, 64]
-
-    write_array_to_disk_chunked(source,  'goo', target_chunks)
-
-
-@app.command()
 def validate_ome_zarr_thing(url: str):
     from .thing import open_ome_zarr_thing
 
